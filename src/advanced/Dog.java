9d51f65b6486467e400bb8aeba782f6add5bf900
Ilya Garkusha <ilja.garkusha94@gmail.com>
2019-12-01 10:37:24 +0200

    started advanced features

---
+++ src/advanced/Dog.java
@@ -0,0 +1,46 @@
+package advanced;
+
+public class Dog {
+    private String name;
+    private int age;
+    private boolean isMale;
+    private double weigth;
+
+    public Dog(String name, int age, boolean isMale, int weigth) {
+        this.name = name;
+        this.age = age;
+        this.isMale = isMale;
+        this.weigth = weigth;
+    }
+
+    public Dog(int age) {
+        Dog aDog = new Dog("Name", 20, false, 60);
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age <= 0) {
+            System.out.println("Age cannot be below 0. Please set right age");
+        }
+        this.age = age;
+    }
+
+    public double getWeigth() {
+        if (weigth > 0){
+            return weigth;
+        }
+        else {
+            System.out.println("Weigth cannot be below 0. Please set right age");
+        }
+        return 0;
+
+    }
+
+    public void setWeigth(double weigth) {
+        this.weigth = weigth;
+    }
+}
